e79a0a87c3b514f4cca8654957902ba5e2ce8f9d
DmMov <dmmov.live@gmail.com>
2020-09-19 22:17:50 +0300

    Fixed some design bugs

--- client/src/components/LinkCard/LinkCard.jsx
+++ client/src/components/LinkCard/LinkCard.jsx
@@ -2,9 +2,15 @@ import React from 'react';
 import './LinkCard.scss';
 
 export const LinkCard = ({ link: { _id, to, from, clicks, date } }) => <div className="linkCard">
-  <h1 className="linkCard__title">Detail of the link ⇒ <span className="highlight">{_id}</span></h1>
-  <p className="linkCard__linkWrap linkCard__to">Your link: <a className="highlight link" href={to} target="_blank" rel="noopener noreferrer">{to}</a></p>
-  <p className="linkCard__linkWrap linkCard__from">From: <a className="highlight link" href={from} target="_blank" rel="noopener noreferrer">{from}</a></p>
+  <h3 className="linkCard__title">Detail of the link ⇒ <span className="highlight">{_id}</span></h3>
+  <p className="linkCard__linkWrap linkCard__to">
+    Your link: <a className="highlight link" href={to} target="_blank" rel="noopener noreferrer">{to.split('//')[1]}</a>
+  </p>
+  <p className="linkCard__linkWrap linkCard__from">
+    From: <a className="highlight link" href={from} target="_blank" rel="noopener noreferrer">{from.split('//')[1]}</a>
+  </p>
   <p className="linkCard__clicks">Clicks: <strong className="digit">{clicks}</strong></p>
-  <p className="linkCard__creadedAt">Created at: <strong className="digit">{new Date(date).toLocaleDateString()}</strong></p>
+  <p className="linkCard__creadedAt">
+    Created at: <strong className="digit">{new Date(date).toLocaleDateString()}</strong>
+  </p>
 </div>;
